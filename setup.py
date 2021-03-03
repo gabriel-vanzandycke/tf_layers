0b007b00bd6d06dde55c5f6fb96d53e1c05d6f65
Gabriel Van Zandycke <18050620+gabriel-vanzandycke@users.noreply.github.com>
2021-03-03 12:57:28 +0000

    increase release version number

--- setup.py
+++ setup.py
@@ -8,9 +8,10 @@ setup(
     licence="LGPL",
     python_requires='>=3.6',
     description="Some tensorflow layers",
-    version='0.0.2',
+    version='0.1.0',
     packages=find_packages(),
     install_requires=[
-        "tensorflow"
+        "tensorflow",
+        "numpy"
     ],
 )
